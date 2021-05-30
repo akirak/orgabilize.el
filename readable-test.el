96df209f5db04184be680eacf25290bb43a89ed6
Akira Komamura <akira.komamura@gmail.com>
2021-05-30 13:49:45 +0900

    Add tests

--- readable-test.el
+++ readable-test.el
@@ -62,6 +62,13 @@
              "md:x-5 | language-c")
             :to-equal "c")))
 
+(describe "readable--file-escape-url-1"
+  (it "converts a URL to a path-safe string"
+    (expect (readable--file-escape-url-1 "https://github.com/akirak/elinter/blob/v4/README.org")
+            :to-be-truthy)
+    (expect (readable--file-escape-url-1 "https://www.reddit.com/r/doctorwho/comments/nnm5cq/1_thing_ive_never_understood_about_no_timelords/")
+            :to-be-truthy)))
+
 (describe "readable-url-regexp-for-escaping"
   (it "matches URLs"
     (let ((s "https://security.googleblog.com/2021/05/integrating-rust-into-android-open.html"))
