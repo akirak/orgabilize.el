480d59c6c4b973a3f429d8dbae7b0e3343e676da
Akira Komamura <akira.komamura@gmail.com>
2021-06-03 23:06:44 +0900

    WIP

---
+++ readable-utils.el
@@ -0,0 +1,46 @@
+;;; readable-utils.el --- Utilities -*- lexical-binding: t -*-
+
+(eval-when-compile
+  (defconst readable-url-regexp-for-escaping
+    (rx bol (+ (any "+" alnum)) ":" (* (any "/"))
+        ;; hostname
+        (group (+ (not (any "/"))))
+        ;; path
+        (?  "/")
+        (group (*? (not (any "?#"))))
+        (?  "/")
+        ;; query
+        (?  (or (group "?" (* (not (any "#"))))
+                (and "#" (+ anything))))
+        eol)
+    "Regexp pattern for URLs."))
+
+(defun readable--file-escape-url-1 (url)
+  "Return a path-safe string for URL."
+  (save-match-data
+    (if (string-match readable-url-regexp-for-escaping url)
+        (concat (->> (match-string 1 url)
+                  (replace-regexp-in-string (rx ".") "_"))
+                "_"
+                (->> (match-string 2 url)
+                  (replace-regexp-in-string "/" "_")
+                  (replace-regexp-in-string (rx (not (any "-_" alnum))) "")
+                  (readable--string-take 128))
+                "="
+                (readable--string-take
+                 10 (sha1 (concat (match-string 2 url)
+                                  (match-string 3 url)))))
+      (error "Did not match the URL pattern: %s" url))))
+
+(defsubst readable--ts-format-iso-8601 (time)
+  "Format TIME in the ISO-8601 date time format."
+  (format-time-string "%FT%X%:z" time))
+
+(defsubst readable--string-take (len string)
+  "Take the first LEN characters of STRING."
+  (if (> len (length string))
+      string
+    (substring string 0 len)))
+
+(provide readable-utils)
+;;; readable-utils.el ends here
