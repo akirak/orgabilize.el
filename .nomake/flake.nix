2d1eb87011b2a69031913892a534d0d448ca46a4
Akira Komamura <akira.komamura@gmail.com>
2022-04-16 13:05:02 +0900

    chore: Add nomake

---
+++ .nomake/flake.nix
@@ -0,0 +1,31 @@
+{
+  description =
+    "THIS IS AN AUTO-GENERATED FILE. PLEASE DON'T EDIT IT MANUALLY.";
+  inputs = {
+    dash = {
+      flake = false;
+      owner = "magnars";
+      repo = "dash.el";
+      type = "github";
+    };
+    org-ml = {
+      flake = false;
+      owner = "ndwarshuis";
+      repo = "org-ml";
+      type = "github";
+    };
+    orgabilize = {
+      flake = false;
+      owner = "akirak";
+      repo = "orgabilize.el";
+      type = "github";
+    };
+    "s" = {
+      flake = false;
+      owner = "magnars";
+      repo = "s.el";
+      type = "github";
+    };
+  };
+  outputs = { ... }: { };
+}
---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "";
+
+  inputs = {
+    gnu-elpa = {
+      url = "git+https://git.savannah.gnu.org/git/emacs/elpa.git?ref=main";
+      flake = false;
+    };
+    melpa = {
+      url = "github:akirak/melpa/orgabilize";
+      flake = false;
+    };
+    epkgs = {
+      url = "github:emacsmirror/epkgs";
+      flake = false;
+    };
+    emacs = {
+      url = "github:emacs-mirror/emacs";
+      flake = false;
+    };
+
+    nomake = {
+      url = "github:emacs-twist/nomake";
+      inputs.gnu-elpa.follows = "gnu-elpa";
+      inputs.melpa.follows = "melpa";
+      inputs.epkgs.follows = "epkgs";
+      inputs.emacs.follows = "emacs";
+    };
+  };
+
+  outputs =
+    { self
+    , nomake
+    , ...
+    } @ inputs:
+    nomake.lib.mkFlake {
+      src = ./.;
+      localPackages = [
+        "orgabilize"
+      ];
+    };
+}
