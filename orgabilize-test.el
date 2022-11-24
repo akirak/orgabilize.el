25ca75d3857b1f6285dbb7d09c8fad29d5af86e8
Akira Komamura <akira.komamura@gmail.com>
2022-11-24 20:04:13 +0900

    tests: Drop outdated tests, and make the test suite work

--- orgabilize-test.el
+++ orgabilize-test.el
@@ -1,7 +1,7 @@
 ;;; -*- lexical-binding: t -*-
 
 (require 'buttercup)
-(require 'readable)
+(require 'orgabilize)
 
 (describe "Utilities"
   (describe "orgabilize--string-take"
@@ -11,39 +11,7 @@
       (expect (orgabilize--string-take 5 "Hi")
               :to-equal "Hi")
       (expect (orgabilize--string-take 5 "Good bye")
-              :to-equal "Good ")))
-
-  (describe "orgabilize--origin-buffer"
-    (let ((orgabilize-cache-directory (make-temp-file "readable" 'dir)))
-      (unwind-protect
-          (let ((buf1 (orgabilize--origin-buffer "https://web.dev")))
-            (progn
-              (it "Returns a read-only buffer of the content"
-                (expect (bufferp buf1) :to-be-truthy)
-                (expect (buffer-local-value 'buffer-read-only buf1) :to-be-truthy)
-                (expect (with-current-buffer buf1
-                          (buffer-substring (point-min) 20))
-                        :to-match
-                        (rx bol "<!DOCTYPE html>"))))
-            (it "Returns the same buffer if called repeatedly"
-              (expect (orgabilize--origin-buffer "https://web.dev")
-                      :to-equal buf1)))
-        (delete-directory orgabilize-cache-directory 'recursive)))))
-
-(defun orgabilize-test-read-html (file url)
-  (with-temp-buffer
-    (insert-file-contents file)
-    (goto-char (point-min))
-    (orgabilize--html-content url)))
-
-(describe "HTML DOM analysis"
-  (describe "orgabilize-html-from-xml"
-    (it "analyses dom"
-      (expect (orgabilize-html-from-xml
-               (orgabilize-test-read-html
-                "data/readable/mdn-1.html"
-                "https://developer.mozilla.org/en-US/docs/Web/API/CSS_Object_Model/Managing_screen_orientation"))
-              :to-be-truthy))))
+              :to-equal "Good "))))
 
 (describe "Fragments"
   (describe "orgabilize-document-fragment-title"
